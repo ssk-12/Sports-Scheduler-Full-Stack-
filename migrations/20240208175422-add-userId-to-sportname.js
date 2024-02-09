963b76b52c602482c8da775ce2f3203a6420d848
ssk-12 <ullegadda.srikanta@gmail.com>
2024-02-09 22:06:05 +0530

    pushed database

---
+++ migrations/20240208175422-add-userId-to-sportname.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn('Sportnames','userId',{
+      type:Sequelize.DataTypes.INTEGER
+    })
+
+    await queryInterface.addConstraint('Sportnames',{
+      fields: ['userId'],
+      type:'foreign key',
+      references:{
+        table: 'Users',
+        field:'id'
+      }
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
---
+++ models/sports.js
@@ -0,0 +1,31 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Sports extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Sports.belongsTo(models.User,{
+        foreignKey:'userId',
+      })
+    }
+  }
+  Sports.init({
+    title: DataTypes.STRING,
+    date: DataTypes.DATEONLY,
+    time: DataTypes.TIME,
+    players: DataTypes.STRING,
+    location: DataTypes.STRING,
+    additional: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Sports',
+  });
+  return Sports;
+};
